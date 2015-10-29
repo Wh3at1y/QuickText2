f0d9ae066fb8b4539749d3ceb28bd87a773914a4
Sam Montoya <smon7958@740s-pl-118.local>
2015-10-29 13:44:29 -0600

    Added to Repo

---
+++ app/src/main/java/com/example/smon7958/quicktext2/MainScreenActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.smon7958.quicktext2;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainScreenActivityFragment extends Fragment {
+
+    public MainScreenActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    }
+}
